bda1790b7197e748ed59a4a4d1c91a620c432238
Dr. Markus Voss <voss.markus@gmx.net>
2022-10-24 22:53:17 +0200

    first working version for the Use Case #1.
    No GUI and default configs yet, needs to be configured on startup every time.

--- README.md
+++ README.md
@@ -2,3 +2,65 @@
 Extend single base notes played on a Keyboard or Synthesizer attached via USB MIDI by further notes to form chords according to selected key.
 These further notes are transmitted back to to the sender of the original base note, so that by playing one note on the keyboard or synth you get
 a full chord with just one finger used.
+
+### Use Cases:
+#### Use Case #1:
+You have a keyboard or synthesizer, and you want to play with two hands, left hand chords, right hand melody/leads. You are not as good
+yet with both. That is why you are only able to play a single base note
+with your left hand. Wouldn't it be cool if automatically 2 further notes would be created that form a triad 
+that matches the key you are playing in?
+
+On the Korg Wavestate Synthesizer you could do this be using 3 of the 4 layers it offers, by programming
+each layer to play one of the notes of the triads, using the pitch lane. So e.g. Layer A keeps the pitch
+at 0, while Layers B and C add the third and fifth to the chord by pitching up e.g. 3 semitones and 7 semitones
+for a minor chord. Furthermore, you can configure the key which shall be considered when pitching in the Pitch
+Lane. The drawback is, that this approach consumes 3 of the 4 lanes of the Synthesizer. When you want to play
+a complete song with the Synth, then this approach would not work as you only have one Layer left for all 
+other instruments to play.
+
+Here ChordProvider is the solution. With ChordProvider you only need one Layer for complete chords. You play
+the base note, the base note is received on your computer via USB MIDI, the computer calculates the complementary
+2 notes to create a triad chord. For that, of course, you need to tell your computer which key you want to 
+play in, same as you would do on the Wavestate in the configuration of the Pitch Lane. Furthermore, you need
+to restrict the Keyzone that shall be used, so that the computer does not make chords out of your lead notes.
+
+### Installation
+#### System setup
+- you need a USB-MIDI capable keyboard or synthesizer (like e.g. the Korg Wavestate in my case)
+- connect that synthesizer to your computer (Apple Mac Studio in my case) via USB with Python3 and git installed
+- open a console and then follow the installation instructions
+
+#### Install latest development
+ChordProvider is a Python3 program. So you need to make sure that you have Python3 (e.g. Python 3.8) installed
+on your computer. Furthermore, ChordProvider uses the Python binding of the RtMidi library, which provides
+an API to utilize MIDI devices that are connected to your computer. 
+
+So, in order to use ChordProvider, you need to install the RtMidi binding first, using pip3:
+
+    pip3 install python-rtmidi
+
+Then download the latest version of ChordProvider. To get the latest developments, clone this git repo to a place somewhere on your Mac/Raspberry Pi or other Unix-like
+system:
+
+    git clone https://github.com/DrMarkusVoss/ChordProvider.git
+    cd ChordProvider
+
+Then just execute the Python program:
+
+    python3 chordprovider.py
+
+
+### User's Guide
+- select the port of your Input MIDI device (e.g. your Keyboard or Synthesizer)
+   where you are going to play the base note
+- select the port of your Output MIDI device, where the complementary notes shall
+  played. Typically, for Use Case #1, this is the same device as the Input device
+- enter the key to which the chords shall belong
+- enter the mode of the key to which the chords shall belong
+- on your input device, play the lower key for the range of keys where you are going
+  to play the base notes
+- then play the upper key for the range of keys where you are going to play the base
+  notes
+- Now you are ready to go! You can play the base notes with one finger and ChordProvider 
+  will make a chord out of it, while you play melodies or leads with your other hand/fingers
+  on the rest of the keyboard that is not reserved for the base notes.
--- chordprovider.py
+++ chordprovider.py
@@ -1,28 +1,196 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
 import time
 import rtmidi
 from rtmidi.midiutil import list_input_ports, list_output_ports, open_midiinput
 
-minor_chord = [0,3,7]
-major_chord = [0,4,7]
-dim_chord = [0,3,6]
+minor_chord = [0, 3, 7]
+major_chord = [0, 4, 7]
+dim_chord = [0, 3, 6]
 
 # 0 = wrong, 1 = major, 2 = minor, 3 = dim
 # C, D, E, F, G, A, B
-c_maj_chords =  [1,0,2,0,2,1,0,1,0,2,0,3]
+c_maj_chords = [1, 0, 2, 0, 2, 1, 0, 1, 0, 2, 0, 3]
 
 c_maj_notes = {0: "C", 1: "C#", 2: "D", 3: "D#", 4: "E", 5: "F", 6: "F#", 7: "G", 8: "G#", 9: "A", 10: "A#", 11: "B"}
 
+major_scale_interval_adders = [2, 2, 1, 2, 2, 2, 1]
+minor_scale_interval_adders = [2, 1, 2, 2, 1, 2, 2]
+
+major_scale_intervals = [0, 2, 4, 5, 7, 9, 11]
+minor_scale_intervals = [0, 2, 3, 5, 7, 8, 10]
+
+selected_input_port = 0
+selected_output_port = 0
+selected_key = ""
+selected_mode = 0
+key_offset = 0
+keyzone_lower_bound = 0
+keyzone_upper_bound = 0
+midiin = None
+
+
+
+def chordProviderMainLoop():
+    global midiin
+
+    midiout = rtmidi.MidiOut()
+
+    list_input_ports()
+
+    selectInputPort()
+
+    list_output_ports()
+
+    selectOutputPort()
+
+    selectKey()
+
+    selectMode()
+
+
+    # open MIDI port 3 (which is in my setup a Kork Wavestate)
+    midiout.open_port(selected_output_port)
+
+    try:
+        midiin, port_name = open_midiinput(selected_input_port)
+    except (EOFError, KeyboardInterrupt):
+        sys.exit()
+
+    learnKeyZoneLowerBound()
+
+    learnKeyZoneUpperBound()
+
+    modestr = {1: "major", 2: "minor"}
+
+    print("\nChordProvider is active now!\n\nPlay your base notes on your musical device (e.g. synth).")
+    print("ChordProvider will complement it with 2 or more additional notes\nto form a chord considering the key of: " + selected_key + " " + modestr[selected_mode])
+    print("\nPress Control-C to exit.")
+    try:
+        timer = time.time()
+        while True:
+            msg = midiin.get_message()
+
+            if msg:
+                message, deltatime = msg
+                timer += deltatime
+                # print("[%s] @%0.6f %r" % (port_name, timer, message))
+                midicmd, note, velocity = message
+
+                # complement the received note only if it is in the defined
+                # keyzone, so that you can play on one part the chords, and
+                # on another part of the keyboard the leads, where you do
+                # not want the notes to be complemented to chords.
+                if note >= keyzone_lower_bound and note <= keyzone_upper_bound:
+                    if midicmd == 144:
+                        # print("NOTE ON")
+                        note2, note3 = getChord(note, True)
+                        note_on2 = [0x90, note2, velocity]
+                        note_on3 = [0x90, note3, velocity]
+                        midiout.send_message(note_on2)
+                        midiout.send_message(note_on3)
+                    if midicmd == 128:
+                        # print("NOTE OFF")
+                        note2, note3 = getChord(note)
+                        note_off2 = [0x80, note2, 0]
+                        note_off3 = [0x80, note3, 0]
+                        midiout.send_message(note_off2)
+                        midiout.send_message(note_off3)
+            # time.sleep(0.01)
+    except KeyboardInterrupt:
+        print('')
+    finally:
+        print("Exit.")
+        midiin.close_port()
+        midiout.close_port()
+        del midiin
+        del midiout
+
+
+
+def learnKeyZoneLowerBound():
+    global keyzone_lower_bound, midiin
+
+    print("\nPlay Lowest Key for base note on your device now...")
+    while True:
+        msg = midiin.get_message()
+
+        if msg:
+            message, deltatime = msg
+            midicmd, note, velocity = message
+
+            if midicmd == 144:
+                keyzone_lower_bound = note
+                print(c_maj_notes[note%12])
+                break
+
+
+
+def learnKeyZoneUpperBound():
+    global keyzone_upper_bound, midiin
+
+    print("\nPlay Highest Key for base note on your device now...")
+    while True:
+        msg = midiin.get_message()
+
+        if msg:
+            message, deltatime = msg
+            midicmd, note, velocity = message
+
+            if midicmd == 144:
+                keyzone_upper_bound = note
+                print(c_maj_notes[note%12])
+                break
+
+
+def selectInputPort():
+    global selected_input_port
+    portnrstr = input("Select the input port by typing the corresponding number: ")
+    selected_input_port = int(portnrstr)
+
+
+def selectOutputPort():
+    global selected_output_port
+    portnrstr = input("Select the output port by typing the corresponding number: ")
+    selected_output_port = int(portnrstr)
+
+
+def selectKey():
+    global selected_key
+    global key_offset
+    keystr = input("\nSelect the key in which to produce the chords (e.g. C, D, F, ...): ")
+    selected_key = keystr.capitalize()
+    key_offset = list(c_maj_notes.keys())[list(c_maj_notes.values()).index(selected_key)]
+    #print("offset = " + str(key_offset))
+    pass
+
+
+def selectMode():
+    global selected_mode
+    modestr = input("\nSelect the mode in which to produce the chords, 1=major 2=minor: ")
+    selected_mode = int(modestr)
+
 
 def getChord(note_midi, verbose=False):
+    global selected_mode
 
     note = note_midi % 12
-    #major, minor, diminished?
-    mmd = c_maj_chords[note]
-    chord = [0,0,0]
+
+    # for major key mode
+    if selected_mode == 1:
+        # major, minor, diminished?
+        # offset in semitones relative to C, that is why the
+        # offset can be subtracted from the note that is taken
+        # from the C major scale chords
+        mmd = c_maj_chords[note - key_offset]
+    # for minor key mode
+    elif selected_mode == 2:
+        # C major = A minor; A is 9 semitones above C
+        mmd = c_maj_chords[((note + 9)%12) - key_offset]
+    else:
+        mmd = 99
+
+    chord = [0, 0, 0]
+
     if mmd == 0:
         if verbose:
             print("Wrong note for key and scale!")
@@ -47,85 +215,27 @@ def getChord(note_midi, verbose=False):
     return note2, note3
 
 
-def chordProviderMainLoop():
 
+def testField():
+# some code to explore and test some funcitonality.
+# not used for the productive part.
     midiout = rtmidi.MidiOut()
     available_ports_out = midiout.get_ports()
 
-    #print(available_ports_out)
-    list_input_ports()
-    list_output_ports()
-
-
-
-    # Attempt to open the port
-    if available_ports_out:
-        midiout.open_port(3)
-    else:
-        midiout.open_virtual_port("My virtual output")
+    print(available_ports_out)
 
     note_on = [0x90, 60, 112]
     note_off = [0x80, 60, 0]
     midiout.send_message(note_on)
     time.sleep(0.5)
-    # I tried running the script without having to invoke the sleep function but it doesn't work.
-    # If someone could enlighten me as to why this is, I'd be more than grateful.
     midiout.send_message(note_off)
     note_on = [0x90, 64, 112]
     note_off = [0x80, 64, 0]
     midiout.send_message(note_on)
     time.sleep(5)
-    # I tried running the script     without having to invoke the sleep function but it doesn't work.
-    # If someone could enlighten me as to why this is, I'd be more than grateful.
     midiout.send_message(note_off)
 
-    try:
-        midiin, port_name = open_midiinput(3)
-    except (EOFError, KeyboardInterrupt):
-        sys.exit()
 
-    print("Entering main loop. Press Control-C to exit.")
-    try:
-        timer = time.time()
-        while True:
-            msg = midiin.get_message()
-
-            if msg:
-                message, deltatime = msg
-                timer += deltatime
-                #print("[%s] @%0.6f %r" % (port_name, timer, message))
-                midicmd, note, velocity = message
-
-                if midicmd == 144:
-                    #print("NOTE ON")
-                    note2, note3 = getChord(note, True)
-                    note_on2 = [0x90, note2, velocity]
-                    note_on3 = [0x90, note3, velocity]
-                    midiout.send_message(note_on2)
-                    midiout.send_message(note_on3)
-                if midicmd ==128:
-                    #print("NOTE OFF")
-                    note2, note3 = getChord(note)
-                    note_off2 = [0x80, note2, 0]
-                    note_off3 = [0x80, note3, 0]
-                    midiout.send_message(note_off2)
-                    midiout.send_message(note_off3)
-
-
-
-            time.sleep(0.01)
-    except KeyboardInterrupt:
-        print('')
-    finally:
-        print("Exit.")
-        midiin.close_port()
-        midiout.close_port()
-        del midiin
-        del midiout
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     chordProviderMainLoop()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
